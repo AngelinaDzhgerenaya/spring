bf8a1452142bd6973e63f0adce4b7a2f3856a79b
AngelinaDzhgerenaya <124816373+AngelinaDzhgerenaya@users.noreply.github.com>
2026-01-02 20:57:12 +0300

    POST

--- src/main/java/com/merion/spring/book/service/BookService.java
+++ src/main/java/com/merion/spring/book/service/BookService.java
@@ -12,15 +12,16 @@ import java.util.Random;
 public class BookService {
     static List<BookEntity> bookStorage = new ArrayList<>();
 
-    public  BookService(){
+    public BookService() {
         fillStorage();
     }
-    public  void fillStorage() {
+
+    public void fillStorage() {
         Random random = new Random();
         for (int i = 0; i < 100; i++) {
             BookEntity book = new BookEntity();
             book.setId(i);
-            book.setTitle("Book #" + random.nextInt(100,999));
+            book.setTitle("Book #" + random.nextInt(100, 999));
             book.setDescription("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
             bookStorage.add(book);
         }
@@ -33,4 +34,13 @@ public class BookService {
     public Optional<BookEntity> byId(Integer id) {
         return bookStorage.stream().filter(book -> book.getId().equals(id)).findFirst();
     }
+
+    public BookEntity create(String title, String description) {
+        BookEntity book = new BookEntity();
+        book.setId(bookStorage.size());
+        book.setTitle(title);
+        book.setDescription(description);
+        return book;
+    }
+
 }
